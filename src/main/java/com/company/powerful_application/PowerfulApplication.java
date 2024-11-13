e047710a04d976967b042975635b475673b9bdd4
luangf <luangf222@gmail.com>
2024-11-12 21:23:14 -0300

    feat: init project

---
+++ src/main/java/com/company/powerful_application/PowerfulApplication.java
@@ -0,0 +1,13 @@
+package com.company.powerful_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/powerful_application/PowerfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.powerful_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
